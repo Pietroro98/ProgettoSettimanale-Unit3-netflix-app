ae335595863609bcb31e57f4e2c9d688d650a26e
Pietroro98 <pietroro98@gmail.com>
2024-08-23 17:14:45 +0200

    modif

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Container, Row, Col, Button,  } from 'react-bootstrap';
-import 'bootstrap-icons/font/bootstrap-icons.css'; 
+
 
 const AccountSettings = () => {
   return (
